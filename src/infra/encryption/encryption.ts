9d6ba783a3b3c5279d22c8303f63b7c6e3cb8300
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-04-25 23:35:53 +0200

    code refactor

--- src/infra/encryption/encryption.ts
+++ src/infra/encryption/encryption.ts
@@ -1,15 +1,16 @@
-const bcrypt = require('bcrypt')
+/*eslint-disable*/
+import bcrypt from 'bcrypt';
 
-const encryptPassword = (password) => {
+const encryptPassword = (password: any) => {
   const salt = bcrypt.genSaltSync()
   return bcrypt.hashSync(password, salt)
 }
 
-const comparePassword = (password, encodedPassword) => {
+const comparePassword = (password: any, encodedPassword: any) => {
   return bcrypt.compareSync(password, encodedPassword)
 }
 
-module.exports = {
+export default {
   encryptPassword,
   comparePassword
 }
