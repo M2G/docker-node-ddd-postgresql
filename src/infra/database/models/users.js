9054a015936c4a1e3aa03f5706f35954e10c7b3d
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-05-03 15:29:37 +0200

    add models

---
+++ src/infra/database/models/users.js
@@ -0,0 +1,33 @@
+/*eslint-disable*/
+const City = require('./city');
+
+const table = "store";
+//@ts-ignore
+module.exports = (sequelize, DataTypes) => {
+ const Store = sequelize.define(table, {
+   store_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+   name: {
+      type: DataTypes.STRING(250),
+      allowNull: false
+    },
+   city_id: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+   },
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+    classMethods: {
+      associate() {}
+    }
+  });
+
+  Store.hasMany(City(), { foreignKey: 'fk_city', foreignKeyConstraint: true });
+
+  return City;
+
+};
