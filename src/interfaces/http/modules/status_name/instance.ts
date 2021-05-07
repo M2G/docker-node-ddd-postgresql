066563b5d1476dedc816e682311760a475b8ef14
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-05-07 22:21:17 +0200

    bind repository to controller - status_name

---
+++ src/interfaces/http/modules/status_name/instance.ts
@@ -0,0 +1,25 @@
+import container from '../../../../container';
+import { get, getOne, post, put, remove } from '../../../../app/users';
+
+export default () => {
+  const { cradle } = container;
+  const {
+ repository: {
+   statusNameRepository
+    }
+  } = cradle;
+
+  const getUseCase = get({statusNameRepository});
+  const getOneUseCase = getOne({statusNameRepository});
+  const postUseCase = post({statusNameRepository});
+  const putUseCase = put({statusNameRepository});
+  const deleteUseCase = remove({statusNameRepository});
+
+  return {
+    deleteUseCase,
+    getOneUseCase,
+    getUseCase,
+    postUseCase,
+    putUseCase
+  };
+};
