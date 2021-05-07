99a0e5d53a9cf06fd965291004a3ab0873d625fc
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-05-07 22:22:50 +0200

    bind repository to controller - sale

--- src/interfaces/http/modules/sale/instance.ts
+++ src/interfaces/http/modules/sale/instance.ts
@@ -1,19 +1,19 @@
 import container from '../../../../container';
-import { get, getOne, post, put, remove } from '../../../../app/users';
+import { get, getOne, post, put, remove } from '../../../../app/sale';
 
 export default () => {
   const { cradle } = container;
   const {
  repository: {
-   statusNameRepository
+   saleRepository
     }
   } = cradle;
 
-  const getUseCase = get({statusNameRepository});
-  const getOneUseCase = getOne({statusNameRepository});
-  const postUseCase = post({statusNameRepository});
-  const putUseCase = put({statusNameRepository});
-  const deleteUseCase = remove({statusNameRepository});
+  const getUseCase = get({saleRepository});
+  const getOneUseCase = getOne({saleRepository});
+  const postUseCase = post({saleRepository});
+  const putUseCase = put({saleRepository});
+  const deleteUseCase = remove({saleRepository});
 
   return {
     deleteUseCase,
