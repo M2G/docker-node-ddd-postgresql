f5c85631c3649cc76181c7f6dcf55d04445cdc24
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-05-08 01:03:16 +0200

    add and bind controller to router

--- src/interfaces/http/router.ts
+++ src/interfaces/http/router.ts
@@ -7,7 +7,14 @@ import httpLogger from './middlewares/http_logger';
 import errorHandler from './middlewares/error_handler';
 // controller
 import * as index from '../http/modules';
-import * as post from '../http/modules/post';
+import * as product from '../http/modules/product';
+import * as country from '../http/modules/country';
+import * as city from '../http/modules/city';
+import * as store from '../http/modules/store';
+import * as users from '../http/modules/users';
+import * as status_name from '../http/modules/status_name';
+import * as sale from '../http/modules/sale';
+import * as order_status from '../http/modules/order_status';
 
 export default ({ config, logger, database }: any) => {
   // console.log('database', database);
@@ -21,12 +28,18 @@ export default ({ config, logger, database }: any) => {
       allowedHeaders: ['Content-Type', 'Authorization'],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       origin: ['http://localhost:3000', 'http://localhost:3001'],
-
     }))
     .use(bodyParser.json());
 
   router.use('/', index.default());
-  router.use('/api/posts', post.default().router);
+  router.use('/api/product', product.default().router);
+  router.use('/api/country', country.default().router);
+  router.use('/api/city', city.default().router);
+  router.use('/api/store', store.default().router);
+  router.use('/api/users', users.default().router);
+  router.use('/api/status_name', status_name.default().router);
+  router.use('/api/sale', sale.default().router);
+  router.use('/api/order_status', order_status.default().router);
   router.use(partialRight(errorHandler, [logger, config]));
 
   return router;
