32c4216f8d041ce18ce400a3d4c7ceff1aab296b
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-05-13 23:58:56 +0200

    use belongsTo for OrderStatus, instead of hasOne

--- src/infra/database/models/order_status.js
+++ src/infra/database/models/order_status.js
@@ -3,9 +3,67 @@
 //  const Status = require('./status_name');
 
 const table = "order_status";
+const table_status_name = "status_name";
+const table_sale = "sale";
 //@ts-ignore
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define(table, {
+module.exports = (sequelize, DataTypes) => {
+
+  const Status = sequelize.define(table_status_name, {
+    status_name_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    status_name: {
+      type: DataTypes.STRING(450),
+      allowNull: false
+    },
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+    classMethods: {
+      associate() {}
+    }
+  });
+
+  const Sale = sequelize.define(table_sale, {
+    sale_id: {
+      type: DataTypes.STRING(200),
+      primaryKey: true,
+      allowNull: false
+    },
+    amount: {
+      type: DataTypes.DOUBLE(20,3),
+      allowNull: false
+    },
+    date_sale: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'product', key: 'product_id'},
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'product_id'},
+    },
+    store_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'store', key: 'store_id'},
+    },
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+    classMethods: {
+      associate() {}
+    }
+  });
+
+  const OrderStatus = sequelize.define(table, {
    order_status_id: {
       type: DataTypes.STRING(200),
       primaryKey: true,
@@ -33,11 +91,10 @@ module.exports = (sequelize, DataTypes) =>
     }
   });
 
- /*
-  OrderStatus.hasOne(Sale(), { foreignKey: 'fk_sale', foreignKeyConstraint: true });
-  OrderStatus.hasOne(Status(), { foreignKey: 'fk_status_name', foreignKeyConstraint: true });
+  OrderStatus.belongsTo(Sale, { foreignKey: 'fk_sale', foreignKeyConstraint: true });
+  OrderStatus.belongsTo(Status, { foreignKey: 'fk_status_name', foreignKeyConstraint: true });
 
   return OrderStatus;
 
 };
-*/
+
