c44a714e62cb7fd3673dd0d8c9f65b76cc4ac305
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2022-01-27 02:28:46 +0100

    add tests unit sale

---
+++ test/unit/app/sale/delete_sale.spec.ts
@@ -0,0 +1,51 @@
+/*eslint-disable*/
+import deleteUseCase from 'app/city/delete';
+
+describe('App -> City -> Delete', () => {
+  const storeId = 1;
+  let useCase: any;
+  const mockData = [{
+    store_id: storeId
+  }];
+
+  describe('Success path', () => {
+    beforeEach(() => {
+      const MockRepository = {
+        destroy: () => mockData
+      };
+
+      useCase = deleteUseCase({
+        storeRepository: MockRepository
+      });
+    });
+
+    it('should display the user on success', async () => {
+      const user = await useCase.remove({ id: storeId });
+      expect(user).toEqual(mockData);
+    });
+  });
+
+  describe('Fail path', () => {
+    beforeEach(() => {
+      const MockRepository = {
+        destroy: async () => Promise.reject('Error')
+      };
+
+      useCase = deleteUseCase({
+        storeRepository: MockRepository
+      });
+    });
+
+    it('should display error on rejection', async () => {
+
+      let error;
+      try {
+        await useCase.remove({ id: storeId });
+      } catch (e) {
+        // error = e.message;
+        error = e;
+      }
+      expect(error).toEqual('Error');
+    });
+  });
+});
